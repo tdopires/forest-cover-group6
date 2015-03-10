da7eb1bc4b034c8b9e8725c9e7c07116b10f38c6
MariaMatthes <maria.matthes@gmail.com>
2015-03-10 13:14:45 +0100

    SVM

---
+++ classifiers/SVM.py
@@ -0,0 +1,33 @@
+# This is just sample code we used to get started on the use of ScikitLearn
+# It was found here, at this topic: http://bit.ly/1FwEoOF
+# To run it, use the command: python random_forest.py
+
+import os
+import pandas as pd
+from sklearn import multiclass
+from sklearn.svm import LinearSVC
+
+if __name__ == "__main__":
+  loc_train = "../data/train.csv"
+  loc_test = "../data/test.csv"
+  loc_submission = os.path.normpath("C:/Users/Maria Matthes/Documents/GitHub/forest-cover-group6/data/result.csv")
+
+  df_train = pd.read_csv(loc_train)
+  df_test = pd.read_csv(loc_test)
+
+  feature_cols = [col for col in df_train.columns if col not in ['Cover_Type','Id']]
+
+  X_train = df_train[feature_cols]
+  X_test = df_test[feature_cols]
+  y = df_train['Cover_Type']
+  test_ids = df_test['Id']
+
+  clf = multiclass.OneVsRestClassifier(LinearSVC())
+  clf.multilabel = True
+
+  clf.fit(X_train, y)
+
+  with open(loc_submission, "wb") as outfile:
+    outfile.write("Id,Cover_Type\n")
+    for e, val in enumerate(list(clf.predict(X_test))):
+      outfile.write("%s,%s\n"%(test_ids[e],val))
--- classifiers/random_forest.py
+++ classifiers/random_forest.py
@@ -2,13 +2,14 @@
 # It was found here, at this topic: http://bit.ly/1FwEoOF
 # To run it, use the command: python random_forest.py
 
+import os
 import pandas as pd
 from sklearn import ensemble
 
 if __name__ == "__main__":
   loc_train = "../data/train.csv"
   loc_test = "../data/test.csv"
-  loc_submission = "/tmp/result.csv"
+  loc_submission = os.path.normpath("C:/Users/Maria Matthes/Documents/GitHub/forest-cover-group6/data/result.csv")
 
   df_train = pd.read_csv(loc_train)
   df_test = pd.read_csv(loc_test)
@@ -19,11 +20,11 @@ if __name__ == "__main__":
   X_test = df_test[feature_cols]
   y = df_train['Cover_Type']
   test_ids = df_test['Id']
-  
+
   clf = ensemble.RandomForestClassifier()
 
   clf.fit(X_train, y)
-  
+
   with open(loc_submission, "wb") as outfile:
     outfile.write("Id,Cover_Type\n")
     for e, val in enumerate(list(clf.predict(X_test))):
