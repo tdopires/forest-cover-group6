b1bab896170337db7c32e8cb0c3be9aa581f3daf
tdopires <tdopires@gmail.com>
2015-03-29 23:13:58 +0200

    Last modifications

--- classifiers/SVM.py
+++ classifiers/SVM.py
@@ -1,8 +1,5 @@
 from base_classifier import MLClassifier
 
-#from sklearn import multiclass
-#from sklearn.svm import LinearSVC
-
 from sklearn import svm
 from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import train_test_split, StratifiedKFold
@@ -14,27 +11,14 @@ class SVM(MLClassifier):
     MLClassifier.__init__(self, "svm", train_file, test_file, output_folder)
 
   def run(self):
-    from sklearn.datasets import make_blobs
-    X, y = make_blobs(n_samples=200, n_features=2, 
-                      centers=((0,0), (4, 4)),
-                      cluster_std=1.0)
-    print 'X', len(X)
-    print 'y', len(y)
-
+   
     (train_instances, train_classes, test_instances, test_ids) = self.parse_data()
 
-    #clf = multiclass.OneVsRestClassifier(LinearSVC())
-    #clf.multilabel = True
-    #clf.fit(train_instances, train_classes)
-    #result_class_with_labels = clf.predict(test_instances)
-    print 'train_instances' + str(len(train_instances))
-    print 'test_instances' + str(len(test_instances))
-
-    X_train, X_test, y_train, y_test = train_test_split(train_instances, test_instances, test_size=0.25)
+    (X_train, X_test, y_train) = (train_instances, test_instances, train_classes)
 
     svc = svm.SVC(class_weight='auto')
-    param_grid = {'kernel': ['poly'],
-                  'C': [1e0, 1e1, 1e2, 1e3, 1e4]}
+    param_grid = {'kernel': ['rbf'], #'poly', 'rbf', 'sigmoid'
+                  'C': [1e0, 1e1, 1e2, 1e3, 1e4]} #
     strat_2fold = StratifiedKFold(y_train, n_folds=2)
     print "    Parameters to be chosen through cross validation:"
     for name, vals in param_grid.iteritems():
@@ -43,7 +27,5 @@ class SVM(MLClassifier):
     clf.fit(X_train, y_train)
     print "== Best Parameters:", clf.best_params_
     result_class_with_labels = clf.predict(X_test)
-    acc = len(np.where(result_class_with_labels == y_test)[0]) / float(len(y_pred))
-    print "== Accuracy:", acc
     
     self.output_result_to_file(test_ids, result_class_with_labels)
